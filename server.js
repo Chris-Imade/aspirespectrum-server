d945d5d09c5164a146df54e5b7d9ac711368d5bb
@Chrisdevcode <jephthahimade@gmail.com>
2025-08-06 15:46:01 +0100

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const { sendContactMail } = require('./mailer');
 const app = express();
 const port = 3000;
 
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'https://aspirespectrum.pages.dev',
+  'https://www.aspirespectrum.pages.dev',
+  'https://aspirespectrum.ca',
+  'https://www.aspirespectrum.ca'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.post('/contact', async (req, res) => {
